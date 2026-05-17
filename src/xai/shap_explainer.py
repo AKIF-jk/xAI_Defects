a71043a81ddacc8017e3e9b00a68d02febc90391
AKIF-jk <i230583@isb.nu.edu.pk>
2026-05-17 13:10:36 +0500

    fix: update masker initialization in PatchSHAPExplainer to use segment map clusters

--- src/xai/shap_explainer.py
+++ src/xai/shap_explainer.py
@@ -67,8 +67,11 @@ class PatchSHAPExplainer:
         image_numpy = self._validate_image_numpy(image_numpy)
         h, w = image_numpy.shape[:2]
         segment_map = self._create_segment_map(h, w)
-        segment_map = np.repeat(segment_map[:, :, np.newaxis], 3, axis=2)
-        masker = shap.maskers.Image("inpaint_telea", segment_map)
+        masker = shap.maskers.Image(
+            "inpaint_telea",
+            image_numpy.shape,
+            clusters=segment_map,
+        )
         explainer = shap.Explainer(self._predict, masker)
         logger.debug("Computing SHAP values with %d evaluations over %d features...", n_evals, self.grid_size ** 2)
         shap_values = explainer(image_numpy[np.newaxis], max_evals=n_evals)
